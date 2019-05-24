372179321942805ddd0bb36f160ca0e31d429662
Warefelt <45390296+Warefelt@users.noreply.github.com>
2019-05-24 16:40:50 +0200

    Delete objtochbor.c

--- objtochbor.c
+++
@@ -1,9 +0,0 @@
-int objtouchesborder(POBJECT o){
-    if((o->posx < 1) || (o->posx + o->geo->sizex) > 128 ){
-        return 1;
-    }
-    else if((o->posy < 1) || (o->posy + o->geo->sizey) > 64){
-        return 2;
-    }
-    return 0;
-}
