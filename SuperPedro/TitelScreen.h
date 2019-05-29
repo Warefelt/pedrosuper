f218f4d00f38d7c97c44604c42e05a542c561a49
Warefelt <45390296+Warefelt@users.noreply.github.com>
2019-05-29 11:08:32 +0200

    Update and rename TitelScreen.h to TitleScreen.h

--- SuperPedro/TitelScreen.h
+++
@@ -1,7 +0,0 @@
-#ifndef TITELSCREEN_H
-#define TITELSCREEN_H
-
-void drawTitleScreen(void)
-
-
-#endif // TITELSCREEN_H
---
+++ SuperPedro/TitleScreen.h
@@ -0,0 +1,7 @@
+#ifndef TITLESCREEN_H
+#define TITLESCREEN_H
+
+void drawTitleScreen(void)
+
+
+#endif // TITLESCREEN_H
